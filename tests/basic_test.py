013d0068e41f87c0b8702c1b4ecd92c26f5aeedc
Eric Zhang <ekzhang1@gmail.com>
2022-10-25 23:38:27 -0400

    Add JAX to dependencies

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,5 +1,21 @@
 from pipejax import add
 
+import numpy as np
+import jax
+import jax.numpy as jnp
+
 
 def test_add():
     assert add(1, 2) == 3
+
+
+def test_jax_works():
+    assert np.add(1, 2) == 3
+    np.testing.assert_allclose(
+        jax.grad(jnp.sum)(jnp.array([1.0, 2, 3])),
+        np.array([1.0, 1, 1]),
+    )
+    np.testing.assert_allclose(
+        jax.jit(jax.grad(lambda x: x[0] - x[1]))(jnp.array([1.0, 2, 3])),
+        np.array([1.0, -1, 0]),
+    )
